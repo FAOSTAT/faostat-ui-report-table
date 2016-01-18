39959d632b7c54d8b5a9a0f487be71db863f8495
simonemurzilli <simone.murzilli@fao.org>
2016-01-18 16:15:58 +0100

    init module

---
+++ nls/es/translate.js
@@ -0,0 +1,35 @@
+/*global define*/
+define({
+
+    "project_name" : "FENIX",
+
+    // Btn and search
+    "btn_go": "View Data",
+    "btn_reset": "Reset",
+    "courtesy_message" : "No results",
+    "no_results" : "No Results",
+    "selector_select_all" : "Select All",
+    "selector_select_none" : "Select None",
+
+    "results": "Results",
+    "group": "Group",
+    "groups": "Groups",
+    "domain": "Domain",
+    "domains": "Domains",
+    "item": "Item",
+    "element": "Element",
+    "regions": "Regions",
+    "area": "Area",
+    "year": "Year",
+    no_data_available: 'No data available.',
+    faostat_domains: 'FAOSTAT Domains',
+    loading: 'Loading',
+    warning: 'Warning',
+    export_data: 'Export Data',
+    next: "Next",
+    previous: "Previous",
+    first: "First",
+    last: "Last",
+    please_select_an_option: "Please select an option"
+
+});
